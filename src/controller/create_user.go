9f2e3b60cd52483f5a7d1b35a4610addbdec3e73
Thiago <tpgoncalvesc@gmail.com>
2024-10-28 16:41:17 -0300

    Adding descriptive fields to error validation

--- src/controller/create_user.go
+++ src/controller/create_user.go
@@ -2,21 +2,22 @@ package controller
 
 import (
 	"fmt"
+	"log"
 
 	"github.com/gin-gonic/gin"
-	"github.com/thpgoncalves/GoLang-CRUD-Application/src/configuration/rest_err"
+	"github.com/thpgoncalves/GoLang-CRUD-Application/src/configuration/validation"
 	"github.com/thpgoncalves/GoLang-CRUD-Application/src/controller/model/request"
 )
 
 func CreateUser(c *gin.Context) {
-	
+	log.Println("Init CreateUser controller")
 	var userRequest request.UserRequest
 
 	if err:= c.ShouldBindBodyWithJSON(&userRequest); err != nil {
-		restErr := rest_err.NewBadRequestError(
-			fmt.Sprintf("There are incorrect fields, error=%s", err.Error()))
+		log.Printf("Error trying to marshal object, error=%s\n", err.Error())
+		errRest := validation.ValidateUserError(err)
 			
-		c.JSON(restErr.Code, restErr)
+		c.JSON(errRest.Code, errRest)
 		return 
 	}
 
